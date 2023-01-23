2ed971c5af4f8e2017e72df2c74baf6ceb47ef04
arthur-art <arthurteixeira.guts@gmail.com>
2023-01-23 01:59:05 -0300

    Trabalhando com rabbitmq, desenvolvendo endpoint para publicar uma fila

---
+++ WebApiRabbitmq/WebApiRabbitmq/Controllers/OrderController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+using WebApiRabbitmq.Domain;
+
+namespace WebApiRabbitmq.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult InserOrder(Order order)
+        {
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "orderQueue",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = JsonSerializer.Serialize(order);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "orderQueue",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                return Accepted(order);
+            }catch(Exception ex)
+            {
+                _logger.LogError("Erro ao tentar criar um novo pedido", ex);
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
